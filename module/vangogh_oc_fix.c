bd69c9373aa009bb29dbb63e4ed870ef5bee51eb
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-05-30 15:53:21 +0200

    Formatting

--- module/vangogh_oc_fix.c
+++ module/vangogh_oc_fix.c
@@ -48,15 +48,17 @@ static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
         pr_err("smu_context does look right. Refusing to modify amdgpu smu limits");
         return 0;
     }
+
     prev = smu->cpu_default_soft_max_freq;
 
-    if( prev != cpu_default_soft_max_freq ) {
-        pr_info("Setting cpu_default_soft_max_freq from %d to %d", prev, cpu_default_soft_max_freq);
-        // Prolly don't need the lock, but w/e
-        mutex_lock(&smu->message_lock);
-        smu->cpu_default_soft_max_freq = cpu_default_soft_max_freq;
-        mutex_unlock(&smu->message_lock);
-    }
+    if(prev != cpu_default_soft_max_freq)
+        return;
+
+    pr_info("Setting cpu_default_soft_max_freq from %d to %d", prev, cpu_default_soft_max_freq);
+    // Prolly don't need the lock, but w/e
+    mutex_lock(&smu->message_lock);
+    smu->cpu_default_soft_max_freq = cpu_default_soft_max_freq;
+    mutex_unlock(&smu->message_lock);
 #endif
     return 0;
 }

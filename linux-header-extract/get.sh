406ee5e4493c7ed7fc3936661cbfd285e222ccc4
lividhen <jaredrskinner@gmail.com>
2024-03-03 00:24:10 -0700

    bad syntax

--- linux-header-extract/get.sh
+++ linux-header-extract/get.sh
@@ -1,7 +1,8 @@
-export UNAME=$(uname -r)
-export VERSION=$(echo $(UNAME) |awk -F"-" '{print $NF}')
+UNAME=$(uname -r)
+VERSION=$(echo $UNAME |awk -F"-" '{print $NF}')
 CUT=${UNAME/-/.}
 CUT=${CUT/-neptune-$VERSION/}
 curl -L https://steamdeck-packages.steamos.cloud/archlinux-mirror/sources/jupiter-main/linux-neptune-${VERSION}-${CUT}.src.tar.gz -o linux-pkg.tar.gz
+tar -xvf linux-pkg.tar.gz
 mv ./linux-neptune-* ./linux-pkg
 rm ./linux-pkg.tar.gz
